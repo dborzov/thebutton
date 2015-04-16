ab7183a9cf813de98dafb229dd97bdd2e7c4769d
Peter Borzov <peter.borzov@gmail.com>
2015-04-15 22:58:32 -0400

    Move endpoints defs to app/endpoints.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,3 +8,5 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
+
+import endpoints
---
+++ app/endpoints.py
@@ -0,0 +1,28 @@
+from flask import render_template, request, redirect
+from app import app
+from app.models import Clicker
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/update.json')
+def update():
+    return Clicker.recent_clicker()
+
+
+
+@app.route('/click', methods=['POST'])
+def click_thebutton():
+    button_click = request.get_json()
+    if "username" not in button_click or Clicker.query.filter_by(username=button_click["username"]).first():
+        # user with such a username already clicked
+        return "User with such a name already exists"
+    c = Clicker(
+        username=button_click['username'],
+        clicked=datetime.utcnow())
+    db.session.add(c)
+    db.session.commit()
+    # FIXME: handle and report errors
+    return ":)"
