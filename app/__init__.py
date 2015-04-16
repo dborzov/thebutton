86d5581f77404d2a3c4393faae72ad626ac27508
Peter Borzov <peter.borzov@gmail.com>
2015-04-16 01:36:43 -0400

    Add secret_key stub for session cookies

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@
 
 from flask import Flask, render_template, request, redirect
 from flask.ext.sqlalchemy import SQLAlchemy
+from secrets import SECRET_KEY
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.secret_key = SECRET_KEY
 db = SQLAlchemy(app)
 
 import endpoints
---
+++ app/secrets.py
@@ -0,0 +1,6 @@
+"""
+    In production the access keys must be stored separately. 
+    One simple way would to store them in ENV variables and fetch them here with os.getenv
+"""
+
+SECRET_KEY = 'Well, that is not a very secret key'
